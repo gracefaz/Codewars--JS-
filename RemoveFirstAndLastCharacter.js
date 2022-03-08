fa26f59a1627dc951ba660a22ba5ab7e34547ed8
gracefaz <gracefaz27@gmail.com>
2022-03-08 15:08:14 +0000

    Add task.

--- RemoveFirstAndLastCharacter.js
+++ RemoveFirstAndLastCharacter.js
@@ -1,5 +1,7 @@
+// Task: Create a function that removes the first and last character of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
 function removeChar(str) {
-  if (str.length <= 2) {
+  if (str.length < 2) {
     return str;
   } else {
     return str.substr(1, str.length - 2);
