b422782f9cb862a5ce6a0e81f51198498a8977b7
gracefaz <gracefaz27@gmail.com>
2022-03-09 12:17:09 +0000

    Jenny Secret Message Kata

---
+++ JennySecretMessage.js
@@ -0,0 +1,14 @@
+// Task: Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. Can you help her?
+
+function greet(name) {
+  if (name === "Johnny") {
+    return "Hello, my love!";
+  } else {
+    return `Hello, ${name}!`;
+  }
+}
+
+console.log(greet("Grace"));
+// Prints "Hello, Grace!"
+console.log(greet("Johnny"));
+// Prints "Hello, my love!"
