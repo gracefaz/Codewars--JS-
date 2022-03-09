935ca7b52521b343045c1a5772d49a33f74e58cd
gracefaz <gracefaz27@gmail.com>
2022-03-09 13:14:10 +0000

    String Repeat kata

---
+++ StringRepeat.js
@@ -0,0 +1,7 @@
+// Task: Write a function called repeatStr which repeats the given string exactly n times.
+
+const repeatStr = (n, s) => {
+  return s.repeat(n);
+};
+
+console.log(repeatStr(4, "Grace"));
