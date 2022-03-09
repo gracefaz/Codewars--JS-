2226638c74ac35e2a6e09e509bd2c78185d719e4
gracefaz <gracefaz27@gmail.com>
2022-03-09 11:52:53 +0000

    Opposite Number Kata

---
+++ OppositeNumber.js
@@ -0,0 +1,12 @@
+// Task: Given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+  return -number;
+}
+
+console.log(opposite(-27));
+// Prints 27.
+console.log(opposite(34));
+// Prints -34.
+console.log(opposite(0));
+// Prints -0.
