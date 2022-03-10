e69e998c65d56c752e72c5f2e8a2c4ef1abc3faf
gracefaz <gracefaz27@gmail.com>
2022-03-10 08:24:47 +0000

    Needle in the Haystack kata

---
+++ NeedleInTheHaystack.js
@@ -0,0 +1,16 @@
+// Task: Can you find the needle in the haystack? Write a function findNeedle() that takes an array full of junk but containing one "needle". After your function finds the needle it should return a message (as a string) that says: "found the needle at position " plus the index it found the needle.
+
+// Example: ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'] returns "found the needle at position 5".
+
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] == "needle") {
+      return "found the needle at position " + haystack.indexOf("needle");
+    }
+  }
+}
+
+console.log(
+  findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
+);
+// Prints "found the needle at position 5"
