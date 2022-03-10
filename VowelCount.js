84f3115a88e196ceaa42a82c7ea5f250bfcc7fd0
gracefaz <gracefaz27@gmail.com>
2022-03-10 13:18:09 +0000

    Vowel Count kata

---
+++ VowelCount.js
@@ -0,0 +1,20 @@
+// Task: Return the number (count) of vowels in the given string.
+
+function getCount(str) {
+  var vowelsCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u"
+    ) {
+      vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
+
+console.log(getCount("hi my name is grace"));
+// Prints 6
