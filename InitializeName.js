c6374be96871143c22d5941c3ff9a6c495276888
gracefaz <gracefaz27@gmail.com>
2022-03-17 12:26:12 +0000

    Edit Initialize Name

--- InitializeName.js
+++ InitializeName.js
@@ -1,16 +1,6 @@
 // Task: Some people just have a first name; some people have first and last names and some people have first, middle and last names.
 //       You task is to initialize the middle names (if there is any).
 
-//function initializeNames(name) {
-//let arr = name.split(" ");
-//if (arr.length >= 3) {
-//arr[1] = arr[1][0] + ".";
-//return arr.join(" ");
-//} else {
-//return name;
-//}
-//}
-
 const initializeNames = (name) => {
   let arr = name.split(" ");
   if (arr.length >= 3) {
