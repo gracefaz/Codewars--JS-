457d779b6690f32d15c256965848aead6aba2083
gracefaz <gracefaz27@gmail.com>
2022-03-17 08:56:02 +0000

    FInd the nth digit kata

---
+++ FindNthDigit.js
@@ -0,0 +1,16 @@
+// Task: Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+
+const findDigit = (num, nth) => {
+  if (nth <= 0) {
+    return -1;
+  } else if (num < 0) {
+    num = -num;
+  }
+
+  let str = num.toString();
+  let find = str.charAt(str.length - nth) * 1;
+  return find;
+};
+
+console.log(findDigit(762413, 2));
+// Should print 1.
