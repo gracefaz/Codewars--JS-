bf16b14765fab7d60467b6eb08dcf07c032739c1
gracefaz <gracefaz27@gmail.com>
2022-03-17 11:43:19 +0000

    Task Middle Character

--- MiddleCharacter.js
+++ MiddleCharacter.js
@@ -1,4 +1,4 @@
-// Task:
+// Task: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 function getMiddle(str) {
   if (str.length % 2 !== 0) {
