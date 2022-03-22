de84aab5a2ecb3205cc816b8ecd0aaf188dfa5f5
gracefaz <gracefaz27@gmail.com>
2022-03-22 13:03:00 +0000

    Leet Speak kata

---
+++ LeetSpeak.js
@@ -0,0 +1,46 @@
+// Task: Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+// Example: toLeetSpeak("LEET") returns "1337"
+
+function toLeetSpeak(str) {
+  const alphabet = {
+    A: "@",
+    B: "8",
+    C: "(",
+    D: "D",
+    E: "3",
+    F: "F",
+    G: "6",
+    H: "#",
+    I: "!",
+    J: "J",
+    K: "K",
+    L: "1",
+    M: "M",
+    N: "N",
+    O: "0",
+    P: "P",
+    Q: "Q",
+    R: "R",
+    S: "$",
+    T: "7",
+    U: "U",
+    V: "V",
+    W: "W",
+    X: "X",
+    Y: "Y",
+    Z: "2",
+  };
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == " ") {
+      result += " ";
+    } else {
+      result += alphabet[str[i]];
+    }
+  }
+  return result;
+}
+
+console.log(toLeetSpeak("GRACE FAZ"));
+// Should print "6R@(3"
