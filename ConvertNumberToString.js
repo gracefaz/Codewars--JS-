a60448a050f957abebb835da8083453654fb30ad
Grace Faz <gracefaz27@gmail.com>
2022-04-01 14:08:20 +0100

    Convert Number to String kata

---
+++ ConvertNumberToString.js
@@ -0,0 +1,11 @@
+// Task: We need a function that can transform a number into a string.
+
+// Examples: 123 --> "123"
+//           999 --> "999"
+
+function numberToString(num) {
+  let string = num.toString();
+  return string;
+}
+
+console.log(numberToString(888));
