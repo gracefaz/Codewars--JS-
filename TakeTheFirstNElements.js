1f2f9beb3794c540abfb214307e4553ab274dfea
Grace Faz <gracefaz27@gmail.com>
2022-04-02 15:17:47 +0100

    Take the First N Elements kata

---
+++ TakeTheFirstNElements.js
@@ -0,0 +1,7 @@
+// Task: Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+
+console.log(take([2, 4, 5, 3, 1, 7], 3));
