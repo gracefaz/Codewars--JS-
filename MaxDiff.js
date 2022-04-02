f0a2bb7b725cf77b0246be5a76dde29c9fa699f7
Grace Faz <gracefaz27@gmail.com>
2022-04-02 17:32:50 +0100

    Max Diff kata

---
+++ MaxDiff.js
@@ -0,0 +1,18 @@
+// Task: You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+//       lst contains integers, that means it may contain some negative numbers
+//       if lst is empty or contains a single element, return 0
+//       lst is not sorted
+
+// [1, 2, 3, 4]   returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  returns 7 because 3 - (-4) == 7
+
+function maxDiff(array) {
+  if (array.length == 0 || array.length == 1) {
+    return 0;
+  } else {
+    return Math.max(...array) - Math.min(...array);
+  }
+}
+
+console.log(maxDiff([]));
